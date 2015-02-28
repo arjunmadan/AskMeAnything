bca1aeae5c2491bff5631a20a211962e84afed2c
Arjun Madan <amadan2@ncsu.edu>
2015-02-28 18:38:16 -0500

    Wolfram 2.0

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def hello_monkey():
 	
 	wolfram_content = client.query(content)
 	logging.warning(wolfram_content.results)
-	message = wolfram_content.results.text
+	message = next(wolfram_content.results).text
 
 #	print(message)
 	
