f4adca4c20208c12b480e6666e1ae210c3ae3030
Arjun Madan <amadan2@ncsu.edu>
2015-02-28 20:50:24 -0500

    Wolfram 2.0

--- run.py
+++ run.py
@@ -21,21 +21,9 @@ def hello_monkey():
 		from_number = request.values.get('From', None)
 		content = request.values.get('Body', None)
 		
-	#	content=content.strip()
-	#	flag=True
-	#	for char in content:
-	#		if char.isdigit() == False:
-	#			flag=False
-	#			break
-	#	if flag==False:
-	#		content=content
-				
-	#	elif content.find(" ") == -1:
-	#		content = "define "+content
-		
-
 		wolfram_content = client.query(content)
-		logging.warning(wolfram_content.results)
+
+		logging.warning(type(wolfram_content))
 		if wolfram_content.results:
 			message = next(wolfram_content.results).text
 		else:
@@ -50,13 +38,6 @@ def hello_monkey():
 		resp = twilio.twiml.Response()
 		resp.message(message)
 		return str(resp)
-
-#	print(message)
-	
-	#if from_number in callers:
-	#	message = callers[from_number] + ", thanks for the message!"
-	#else:
-	#	message = "Monkey, thanks for the message!"
 	
 if __name__ == "__main__":
 	app.run(debug=True)
