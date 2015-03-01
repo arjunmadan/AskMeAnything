9d88110849473312d02c6b00fb26845050e3fccb
Arjun Madan <amadan2@ncsu.edu>
2015-03-01 04:54:07 -0500

    Bug fixes

--- run.py
+++ run.py
@@ -34,6 +34,7 @@ def hello_monkey():
 	title_list = ["Definition", "Pronounciation", "Result", "Basic information", "Leadership position", "Notable facts", "Distance", "Company information", "Properties", "Name"]
 	if root.attrib['success'] == "true":
 		for pod in root:
+			logging.warning(pod.tag)
 			if pod.tag == "pod":
 				logging.warning(pod.attrib['title'])
 				for title in title_list:
